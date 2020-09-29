9bb1bf5e3e2873c0779e2901409f624e6ce993f5
郑好 <1223363269@qq.com>
2020-09-29 09:58:11 +0800

    Site updated: 2020-09-29 09:58:10

--- js/main.js
+++ js/main.js
@@ -238,4 +238,17 @@
         sideMenuBoxIsOpen = true;
         Blog.showMobileHeaderMenu(false);
     });
+    // qq and wexin
+    var faQq = document.getElementsByClassName('fa-qq')
+    var faWexin = document.getElementsByClassName('fa-weixin')
+    var divTestQq = document.createElement('div')
+    divTestQq.innerHTML = 'QQ'
+    divTestQq.style.display = 'none'
+    divTestQq.style.width = '500px'
+    divTestQq.style.height = '500px'
+    faQq.appendChild(divTestQq)
+    faQq.onclick = function (e) {
+        e.preventDefaul()
+        divTestQq.style.display = 'block'
+    }
 })(window, document);
